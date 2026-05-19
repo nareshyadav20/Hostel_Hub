21f9f917641a62f0f3033aa0f0ab83695cc97495
nareshyadav20 <nareshnareshyadav20@gmail.com>
2026-05-19 16:53:59 +0530

    fix vercel lightningcss issue

--- frontend/admin/vite.config.js
+++ frontend/admin/vite.config.js
@@ -4,15 +4,25 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+
+  css: {
+    transformer: 'postcss'
+  },
+
   server: {
     port: 5175,
     fs: {
       allow: ['..', '../../packages']
     }
   },
+
   resolve: {
     alias: {
       '@packages': path.resolve(__dirname, '../../packages'),
     }
+  },
+
+  build: {
+    chunkSizeWarningLimit: 2000
   }
 })
